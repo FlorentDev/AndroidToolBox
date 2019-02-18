e2767a1e3adda527a542f6e792e457449a47c2b5
FlorentDev <florentdev@github.com>
2019-02-18 14:00:06 +0100

    Frida detection

--- app/src/main/java/com/isen/salou/androidtoolbox/HomeActivity.kt
+++ app/src/main/java/com/isen/salou/androidtoolbox/HomeActivity.kt
@@ -1,17 +1,40 @@
 package com.isen.salou.androidtoolbox
 
+import android.app.ActivityManager
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-
+import android.system.Os.connect
+import android.system.OsConstants.AF_INET
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_home.*
 import kotlinx.android.synthetic.main.content_home.*
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.i("Frida detection", "Start frida detection")
+        val process = Runtime.getRuntime().exec("ps")
+        val reader = BufferedReader(InputStreamReader(process.inputStream))
+        val buffer = CharArray(4096)
+        val output = StringBuffer()
+        while (reader.read(buffer) > 0) {
+            output.append(buffer, 0, reader.read(buffer))
+        }
+        reader.close()
+
+        process.waitFor()
+
+        if (output.toString().contains("frida")) {
+            Log.i("Frida detection", "Frida Server found!")
+            finish()
+            System.exit(0)
+        }
+
         setContentView(R.layout.activity_home)
         setSupportActionBar(toolbar)
         cyclevie.setOnClickListener { startActivity(Intent(this, LifeCycle::class.java)) }
