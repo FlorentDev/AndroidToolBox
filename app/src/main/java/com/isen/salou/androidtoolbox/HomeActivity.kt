5c45e5ef9a841694f0ce98053f35d887617a0b9f
FlorentDev <florentdev@github.com>
2019-02-19 10:07:01 +0100

    Correction Frida Correction de l'erreur lors de l'authent avec verif Frida

--- app/src/main/java/com/isen/salou/androidtoolbox/HomeActivity.kt
+++ app/src/main/java/com/isen/salou/androidtoolbox/HomeActivity.kt
@@ -1,16 +1,14 @@
 package com.isen.salou.androidtoolbox
 
-import android.app.ActivityManager
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.system.Os.connect
-import android.system.OsConstants.AF_INET
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_home.*
 import kotlinx.android.synthetic.main.content_home.*
 import java.io.BufferedReader
+import java.io.IOException
 import java.io.InputStreamReader
 
 class HomeActivity : AppCompatActivity() {
@@ -18,23 +16,32 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.i("Frida detection", "Start frida detection")
-        val process = Runtime.getRuntime().exec("ps")
-        val reader = BufferedReader(InputStreamReader(process.inputStream))
-        val buffer = CharArray(4096)
-        val output = StringBuffer()
-        while (reader.read(buffer) > 0) {
-            output.append(buffer, 0, reader.read(buffer))
+        try {
+            val process = Runtime.getRuntime().exec("ps")
+            val reader = BufferedReader(InputStreamReader(process.inputStream))
+            val buffer = CharArray(4096)
+            val output = StringBuffer()
+            var read = reader.read()
+            while (read > 0) {
+                output.append(buffer, 0, read)
+                read = reader.read(buffer)
+            }
+            reader.close()
+
+            process.waitFor()
+
+            if (output.toString().contains("frida")) {
+                Log.i("Frida detection", "Frida Server found!")
+                finish()
+                System.exit(0)
+            }
         }
-        reader.close()
-
-        process.waitFor()
-
-        if (output.toString().contains("frida")) {
-            Log.i("Frida detection", "Frida Server found!")
-            finish()
-            System.exit(0)
+        catch(e: IOException){
+            Log.e("Frida", "Erreur IO")
         }
-
+        catch(e: InterruptedException){
+            Log.e("Frida", "Erreur d'excecution")
+        }
         setContentView(R.layout.activity_home)
         setSupportActionBar(toolbar)
         cyclevie.setOnClickListener { startActivity(Intent(this, LifeCycle::class.java)) }
